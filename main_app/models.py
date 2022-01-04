c5a44aef6ef38cbf73ced3082b2c1e437ff648ad
Romeo Encinares <romeoncrs@gmail.com>
2022-01-04 19:14:31 +0800

    Add main_app in Installed_Apps

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
+from django import shortcuts
 from django.db import models
 
 # Create your models here.
+
+class Link(models.Model):
+    url = models.CharField(max_length=1000)
+    shortUrl = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.shortUrl
